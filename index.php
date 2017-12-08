ea7e52b94388469cc79a0f4993dff32ea9a19361
Vadym Polietaiev <va2dim@gmail.com>
2017-12-08 12:49:49 +0200

    +declination

--- index.php
+++ index.php
@@ -1,81 +1,69 @@
 <?php
 
+
 /**
- * @param string $lang - язык категории
- * @param $items_amount - кол-во эл-тов в категории
- * @param $word_single - название категории в ед.ч.
- * @param $word_plural - название категории в множ.ч.
+ * @param $amount - кол-во эл-тов в категории
+ * @param $nominative - название категории в именительном падеже ед./мн.ч.
+ * @param $genetive - название категории в родительском падеже ед.ч.
+ * @param $plural_genetive - название категории в родительском падеже мн.ч.
  * @return string
  *
  */
-function translate($lang, $items_amount, $word_single, $word_plural)
+function translate($amount, $nominative = '', $genetive = '', $plural_genetive = '')
 {
 
-    $amount = $items_amount;
+    $nd = strlen((string) $amount);
+    $divModuleEnding = $amount % (10**($nd-1));
 
-    $ending['ru'] = ['ка'];
-
-
-    $amount_str = (string) $amount;
-    $l = strlen($amount_str);
-    if ($l > 2) {
-        $amount_str = substr($amount_str,-2+$l, strlen($amount_str));
-        $amount = (int) $amount_str;
-    }
-
-    $divModuleEnding = $amount % 10;
-
-    if(('ru' == $lang) && (substr($word_single,0,2) != substr($word_plural,0,2) )) {
-        if (((1 == $divModuleEnding) || (1 == $amount)) && (!((11 == $amount) || (11 == $divModuleEnding)))) {
-            $word = $word_single;
-        } else {
-            if ((in_array($amount, range(2, 4)) || in_array($divModuleEnding,
-                  range(2, 4)))
-              && (!(in_array($amount, range(11, 19)) || (in_array($divModuleEnding,
-                  range(11, 19)))))
-            ) {
-                $word = $word_single . $ending[$lang][0];
-            } else {
-                $word = $word_plural;
-            }
-        }
+    if (((1 == $divModuleEnding) || (1 == $amount)) && (!((11 == $amount) || (11 == $divModuleEnding)))) {
+        $word = $nominative;
     } else {
-        if (1 == $amount) {
-            $word = $word_single;
+        if ((in_array($amount, range(2, 4)) || in_array($divModuleEnding,
+              range(2, 4)))
+          && (!(in_array($amount, range(11, 19)) || (in_array($divModuleEnding,
+              range(11, 19)))))
+        ) {
+            if (!empty($genetive)) {
+                $word = $genetive;
+            } else {
+                $word = $plural_genetive;
+            }
         } else {
-            $word = $word_plural;
+            $word = $plural_genetive;
         }
     }
 
-
-
-    var_dump($items_amount.' '.$word);
-    //die;
-
-    return $items_amount.' '.$word;
+    return $amount.' '.$word;
 }
 
 
-$category['ru'][] = ['ребенок', 'детей'];
-$category['ru'][] = ['очки', 'очков'];
-$category['en'][] = ['child', 'children'];
-$category['en'][] = ['glasses','glasses'];
+$category['ru'][] = ['ребенок', 'ребенка', 'детей'];
+$category['ru'][] = ['очки', '', 'очков'];
+$category['en'][] = ['child', '', 'children'];
+$category['en'][] = ['glasses','','glasses'];
 
-$category['es'][] = ['niño', 'niños'];
-$category['es'][] = ['espectáculos', 'espectáculos'];
+$category['es'][] = ['niño', '', 'niños'];
+$category['es'][] = ['espectáculos', '', 'espectáculos'];
 
 
-for ($i=1;$i<10;$i++) {
-    translate('ru', $i, $category['ru'][0][0],$category['ru'][0][1]);
+for ($i=1;$i<103;$i++) {
+    echo "\n".translate($i, $category['ru'][0][0],$category['ru'][0][1],$category['ru'][0][2]);
 }
 for ($i=1;$i<10;$i++) {
-    translate('ru', $i, $category['ru'][1][0],$category['ru'][1][1]);
-}
-for ($i=1;$i<10;$i++) {
-    translate('en', $i, $category['en'][0][0], $category['en'][0][1]);
+    echo "\n".translate($i, $category['ru'][1][0],$category['ru'][1][1],$category['ru'][1][2]);
 }
 
 for ($i=1;$i<10;$i++) {
-    translate('es', $i, $category['es'][0][0],$category['es'][0][1]);
-    //translate('en', $i, $category['en'][1][0],$category['en'][1][1]);
+    echo "\n".translate($i, $category['en'][0][0], $category['en'][0][1], $category['en'][0][2]);
 }
+for ($i=1;$i<10;$i++) {
+    echo "\n".translate($i, $category['en'][1][0], $category['en'][1][1], $category['en'][1][2]);
+}
+
+for ($i=1;$i<10;$i++) {
+    echo "\n".translate( $i, $category['es'][0][0],$category['es'][0][1], $category['es'][0][2]);
+}
+for ($i=1;$i<10;$i++) {
+    echo "\n".translate($i, $category['es'][1][0],$category['es'][1][1], $category['es'][1][2]);
+}
+
